3efe37415f44c37db113d79268b11c4a97921c5e
bas21gv <bas21gv@gmail.com>
2021-05-01 12:20:03 +0530

    initial code commit

---
+++ src/main/java/com/example/rest/dao/EmployeeDAO.java
@@ -0,0 +1,24 @@
+package com.example.rest.dao;
+
+import com.example.rest.model.Employee;
+import com.example.rest.model.Employees;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class EmployeeDAO {
+    private static Employees list = new Employees();
+
+    static {
+        list.getEmployeeList().add(new Employee(1, "Lokesh", "Gupta", "howtodoinjava@gmail.com"));
+        list.getEmployeeList().add(new Employee(2, "Alex", "Kolenchiskey", "abc@gmail.com"));
+        list.getEmployeeList().add(new Employee(3, "David", "Kameron", "titanic@gmail.com"));
+    }
+
+    public Employees getAllEmployees() {
+        return list;
+    }
+
+    public void addEmployee(Employee employee) {
+        list.getEmployeeList().add(employee);
+    }
+}
